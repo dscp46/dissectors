6b759b36396696a83c1e2fafe88f97b59cb8a537
Zipping through network queues <117170253+dscp46@users.noreply.github.com>
2025-03-18 18:17:55 +0100

    Update dsvt.lua

--- dsvt.lua
+++ dsvt.lua
@@ -1,6 +1,35 @@
--- dsvt.lua
--- D-Star Voice Streaming Protocol
-p_dsvt = Proto ( "dsvt", "D-Star Voice Streaming Protocol")
+ -- dsvt.lua - D-Star Voice Streaming Protocol
+ 
+local proto_shortname = "dsvt"
+local proto_longname =  "D-Star Voice Streaming Protocol"
+p_dsvt = Proto ( proto_shortname, proto_longname)
+
+-------------------------------------------------------------------------------
+-- Enums
+-------------------------------------------------------------------------------
+local miniheader_types = {
+	[0x3]=" Simple Data (D-PRS / PC-to-PC)",
+	[0x4]=" Message Function (radio to radio)",
+	[0x5]=" Radio header retransmission",
+	[0x6]=" Reserved",
+	[0x8]=" Fast Data",
+	[0x9]=" Fast Data",
+}
+
+-------------------------------------------------------------------------------
+-- Fields
+-------------------------------------------------------------------------------
+
+-- My own fields
+local pf_dsvt_header = ProtoField.string ( proto_shortname .. ".sig" , "Signature", base.ASCII)
+local pf_dsvt_mh = ProtoField.uint8 ( proto_shortname .. ".data.mini" , "Miniheader", base.HEX)
+local pf_dsvt_mh_number = ProtoField.uint8 ( proto_shortname .. ".data.mini.number" , "Miniheader Number", base.HEX, miniheader_types, 0xF0)
+local pf_dsvt_mh_sequence = ProtoField.uint8 ( proto_shortname .. ".data.mini.seq" , "Miniheader Sequence", base.HEX, nil, 0x0F)
+local pf_dsvt_dv_data = ProtoField.bytes ( proto_shortname .. ".dv.data" , "DV Data")
+
+p_dsvt.fields = {
+	pf_dsvt_header, pf_dsvt_mh, pf_dsvt_mh_number, pf_dsvt_mh_sequence, pf_dsvt_dv_data
+}
 
 local function is_config_frame( buffer)
 	return buffer(4,1):uint() == 0x10 
@@ -28,31 +57,32 @@ end
 local function decode_data_miniheader( buffer, tree)
 	local len = buffer():len()
 	local miniheader = buffer(0,1):uint()
-	local mh_tree = tree:add( p_dsvt, buffer(0,1), "Miniheader: ")
+	local mh_tree = tree:add( pf_dsvt_mh, buffer(0,1))
+	mh_tree:add( pf_dsvt_mh_number, buffer(0,1))
 	
 	if ( miniheader >= 0x31 and miniheader <= 0x35 ) then
-		mh_tree.text = mh_tree.text .. "Simple Data (D-PRS / PC-to-PC)"
+		--mh_tree.text = mh_tree.text .. ""
 		
 	elseif ( miniheader >= 0x40 and miniheader <= 0x43 ) then
-		mh_tree.text = mh_tree.text .. "Message Function (radio to radio)"
+		--mh_tree.text = mh_tree.text .. "Message Function (radio to radio)"
 		
 	elseif ( miniheader >= 0x51 and miniheader <= 0x55 ) then
-		mh_tree.text = mh_tree.text .. "Radio header retransmission"
+		--mh_tree.text = mh_tree.text .. "Radio header retransmission"
 		tree:add( p_dsvt, buffer(1,2), "Radio header data")
 		
 	elseif ( miniheader == 0x66 ) then
-		mh_tree.text = mh_tree.text .. "No Data"
+		--mh_tree.text = mh_tree.text .. "No Data"
 		
 	elseif ( miniheader >= 0x81 and miniheader <= 0x9C ) then
-		mh_tree.text = mh_tree.text .. "Fast Data"
+		--mh_tree.text = mh_tree.text .. "Fast Data"
 		tree:add( p_dsvt, buffer(1,2), "Payload")
 		
 	elseif ( miniheader == 0xC2 ) then
-		mh_tree.text = mh_tree.text .. "Code squelch"
+		--mh_tree.text = mh_tree.text .. "Code squelch"
 		tree:add( p_dsvt, buffer(1,2), "2 digits for code squelch")
 		
 	else
-		mh_tree.text = mh_tree.text .. "Reserved"
+		-- mh_tree.text = mh_tree.text .. " Reserved"
 		if ( len > 1 ) then
 			local unk = tree:add( p_dsvt, buffer(1), "Undocumented data")
 			unk:add_expert_info( PI_UNDECODED, PI_WARN, "Undocumented data format")
@@ -60,6 +90,8 @@ local function decode_data_miniheader( buffer, tree)
 		
 	end
 end
+
+local p_dsvt_stream_attrs = {}
 
 function p_dsvt.dissector ( buffer, pinfo, tree)
 	-- Validate packet length
@@ -69,26 +101,42 @@ function p_dsvt.dissector ( buffer, pinfo, tree)
 	if( buffer(0,4):string() ~= "DSVT" ) then return end
 	
 	-- Set protocol name
-	--pinfo.cols.protocol = "DPLUS"
+	pinfo.cols.protocol = "DSVT"
 	pinfo.cols.info = "DSVT"
 	
 	-- Variables
-	local stream_id = buffer(12,2);
-	local seq_num = buffer(14,1);
+	local stream_id = buffer(12,2):uint();
+	local seq_num = buffer(14,1):uint();
+	local internal_seq = nil
+	
+	-- Allocate Sequence number
+	if ( p_dsvt_stream_attrs[stream_id] == nil ) then
+		p_dsvt_stream_attrs[stream_id] = {}
+		p_dsvt_stream_attrs[stream_id]["seq"] = 0
+	end
+	
+	-- Initialize stream attributes, allocate internal sequence number, to re-aggregate data
+	if ( pinfo.private["dvst_seq"] == nil ) then
+		pinfo.private["dvst_seq"] = p_dsvt_stream_attrs[stream_id]["seq"]
+		internal_seq = p_dsvt_stream_attrs[stream_id]["seq"]
+		p_dsvt_stream_attrs[stream_id][internal_seq] = {}
+		
+		p_dsvt_stream_attrs[stream_id]["seq"] = p_dsvt_stream_attrs[stream_id]["seq"] + 1
+	end
 	
 	-- Fill the diagnostic tree
 	local subtree = tree:add( p_dsvt, buffer(), "Digital Voice Streaming Protocol")
-	subtree:add( buffer(0,4) , "Signature: " .. buffer(0,4):string())
+	subtree:add( pf_dsvt_header, buffer(0,4))
 	subtree:add( buffer(4,1) , "Frame Type: " .. get_frame_type_string( buffer() ) .. " (0x" .. buffer(4,1) .. ")")
 	subtree:add( buffer(5,3) , "Reserved: " .. buffer(5,3))
 	subtree:add( buffer(8,1) , "Stream type: " .. get_stream_type_string( buffer() ) .. " (0x" .. buffer(8,1) .. ")")
 	subtree:add( buffer(9,3) , "Reserved: " .. buffer(9,3))
-	subtree:add( buffer(12,2), "Stream id: 0x" .. stream_id)
-	subtree:add( buffer(14,1), "Sequence: 0x" .. seq_num)
+	subtree:add( buffer(12,2), string.format( "Stream id: 0x%04X", stream_id))
+	subtree:add( buffer(14,1), string.format( "Sequence: 0x%02X", seq_num))
 	
 	-- Configuration Frame
 	if ( is_voice_stream(buffer()) and is_config_frame(buffer()) ) then
-		pinfo.cols.info = "Stream Configuration SID=" .. stream_id:uint() 
+		pinfo.cols.info = string.format( "Stream Configuration SID=0x%04X", stream_id)
 		-- subtree:add( buffer(,8), ": " .. buffer(,):string())
 		-- Subtree for the flag fields
 		--local subtree_flags = subtree:add( buffer(15,3) , "D-Star flags" )
@@ -104,9 +152,20 @@ function p_dsvt.dissector ( buffer, pinfo, tree)
 	
 	-- AMBE Voice Frame
 	if ( is_voice_stream(buffer()) and is_ambe_voice_frame(buffer()) ) then
-		pinfo.cols.info = " Voice Fragment SID=" .. stream_id:uint() .. " SEQ=0x" .. seq_num .. " [Codec: AMBE]"
+		pinfo.cols.info = string.format( "Voice Fragment SID=0x%04X SEQ=0x%02X [Codec: AMBE]", stream_id, seq_num)
 		local voice_subtree = subtree:add( buffer(15,9), "AMBE voice fragment: " .. buffer(15,9))
-		local data_subtree = subtree:add( buffer(24,3), "DV data fragment: " .. buffer(24,3))
-		decode_data_miniheader( buffer(24,3), data_subtree)
+		subtree:add( p_dsvt, buffer(24,3), "[DV data fragment]")
+		
+		if( bit.band( seq_num, 0x01) == 1 ) then
+			p_dsvt_stream_attrs[stream_id][internal_seq]["data"] = buffer(24,3):bytes():tohex()
+		else
+			if ( p_dsvt_stream_attrs[stream_id][internal_seq-1] ~= nil and p_dsvt_stream_attrs[stream_id][internal_seq-1]["data"] ~= nil ) then
+				local dv_data = ByteArray.new( p_dsvt_stream_attrs[stream_id][internal_seq-1]["data"] )
+				dv_data:append( buffer(24,3):bytes())
+				local tvb_data = dv_data:tvb()
+				local data_subtree = subtree:add( pf_dsvt_dv_data, tvb_data())
+				decode_data_miniheader( tvb_data, data_subtree)
+			end
+		end
 	end
 end
